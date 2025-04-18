adfe76d03f5ba2a0a19062d62c1a0fc7286c8422
Androymm <antonin.froment@hotmail.com>
2025-04-18 11:03:25 +0200

    fix POST ressource new database

--- src/data/accessor/ressourcesAccessor.ts
+++ src/data/accessor/ressourcesAccessor.ts
@@ -1,14 +1,16 @@
 import conn from "../connector/connect";
 
-export const createRessource = async (ressource: { res_nom: string, com_commentaire: string, res_extension:string,  res_auteur: string, cat_categorie: string | null }) => {
-  const query = "INSERT INTO ressource (res_nom, com_commentaire, res_extension, res_auteur, cat_categorie) VALUES (?, ?, ?, ?, ?)";
+export const createRessource = async (ressource: { res_nom: string, com_commentaire: string, res_extension:string,  res_auteur: string, cat_categorie: string, res_lien: string, res_description: string| null }) => {
+  const query = "INSERT INTO ressource (res_nom, com_commentaire, res_extension, res_auteur, cat_categorie, res_lien, res_description) VALUES (?, ?, ?, ?, ?, ?, ?)";
   console.log("Résultat brut de db.query : ", query);
   const result = await conn.execute(query, [
     ressource.res_nom, 
     ressource.com_commentaire, 
     ressource.res_extension,
     ressource.res_auteur, 
-    ressource.cat_categorie
+    ressource.cat_categorie,
+    ressource.res_lien,
+    ressource.res_description
   ]);
   return result;
 };
